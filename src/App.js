4e83addf78cf1535113b117b218b621aa92d6ab7
harrishalisha <noranalisha@gmail.com>
2022-05-20 09:42:44 +0100

    dispath action

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import {useSelector} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 
 
 function App() {
   const counter = useSelector((state)=> state.counter);
+  const dispatch = useDispatch(); //dispatch(action)
 
+  const increment = ()=> { dispatch({type: "INCREMENT"})}
+  const decrement = ()=> { dispatch({type: "DECREMENT"})}
 
   return (
    <div>
      <h1>Counter App</h1>
      <h2>{counter}</h2>  
+
+     <button onClick={increment}>Increment</button>
+     <button onClick={decrement}>Decrement</button>
    </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,21 @@
 import {createStore } from 'redux'
 
 const reducerFn = (state= {counter: 0}, action) => {
+  
+   //Syncronous Function
+   //we should not mutate original state
+
+   if(action.type =="INCREMENT"){
+     return { counter : state.counter+1};
+   }
+   if(action.type === "DECREMENT"){
+      //if(state.counter <= 0){
+      //   return {counter: state.counter};
+      //}
+
+      return {counter: state.counter-1};
+   }
+
    return state;
 }
 const store = createStore(reducerFn);
